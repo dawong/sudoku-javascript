ca47e138a99a8656f8efe9603c481eeb257603bf
dawong <dwong38@gmail.com>
2015-11-15 21:05:26 -0800

    algorithm to generate board

--- game.js
+++ game.js
@@ -1,4 +1,56 @@
-for (var i=0; i<81; i++) {
+/*for (var i=0; i<81; i++) {
   var block = $('#block' + i);
   block.html(i);
+}*/
+
+
+for (var r=0; r<9; r++) {
+  var numPool = [1,2,3,4,5,6,7,8,9];
+  var count = 0;
+  for (var c=0; c<9; c++) {
+   
+    var rand = Math.floor(Math.random() * 9) + 1;
+
+    // if number not available in number pool (not in row)
+    while (numPool.indexOf(rand) == -1) {
+      rand = Math.floor(Math.random() * 9) + 1;
+      count++;
+    }
+
+    // if number already in column
+    if (inColumn(r, c, rand)) {
+      numPool.splice(numPool.indexOf(rand), 1);
+      continue;
+    }
+    
+    // if number already in section
+    if (inSection(r, c, rand)) {
+      numPool.splice(numPool.indexOf(rand), 1);
+      continue;
+    }
+    
+    
+    numPool.splice(numPool.indexOf(rand), 1);
+    var cell = r*9 + c;
+    var block = $('#block' + cell);
+    block.html(rand);
+    
+  }
+console.log("ran through " + count + " times!");
+}
+
+function inSection(row, column, num) {
+  for (var i=0; i<9; i++) {
+    
+  }
+}
+function inColumn(row, column, num) {
+  for (var i=0; i<9; i++) {
+    var cell = row*i + column;
+    var block = $('#block' + cell);
+    if (block.html() == num) {
+      return true;
+    }
+  }
+  return false;
 }
